db3d313d5c8c31cab5b6ec19574677ed07ac1165
Will Rossen <wor101@gmail.com>
2020-09-10 18:47:07 +0000

    Destroyer of Worlds program

--- example.rb
+++ example.rb
@@ -1,1 +1,10 @@
-puts "I am destroyer of worlds!"
+puts "Are you the destroyer of Worlds? "
+answer = gets.chomp
+answer.downcase!
+if answer == "yes"
+  puts "Thats not true! I am the destroyer of Worlds!"
+elsif answer == "no"
+  puts "That's right loser! I am the Destroyer of Worlds!!"
+else
+  puts "Huh? This is a Yes / No question dummy!"
+end 
