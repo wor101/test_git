a36cd9a7018fc0e92a1e5edfa7725ab0aa65c348
Will Rossen <wor101@gmail.com>
2020-09-10 19:51:29 +0000

    "Update with maybe answer

--- example.rb
+++ example.rb
@@ -5,6 +5,8 @@ if answer == "yes"
   puts "Thats not true! I am the destroyer of Worlds!"
 elsif answer == "no"
   puts "That's right loser! I am the Destroyer of Worlds and you are nothing!!"
+elsif answer == "maybe"
+  puts "What? There can be no confusion over who is the destroyer of worlds?!"
 else
   puts "Huh? This is a Yes / No question dummy!"
 end 
