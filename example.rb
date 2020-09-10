0bb112022dcade4f28ef211d0212edac4ab3a6b4
wor101 <wor101@gmail.com>
2020-09-10 15:13:42 -0400

    Update example.rb

--- example.rb
+++ example.rb
@@ -4,7 +4,7 @@ answer.downcase!
 if answer == "yes"
   puts "Thats not true! I am the destroyer of Worlds!"
 elsif answer == "no"
-  puts "That's right loser! I am the Destroyer of Worlds!!"
+  puts "That's right loser! I am the Destroyer of Worlds and you are nothing!!"
 else
   puts "Huh? This is a Yes / No question dummy!"
 end 
